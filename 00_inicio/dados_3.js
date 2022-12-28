cd05a356c7992aac5d1dee3c7a8363160783227a
Patrícia Souza <patricia.sz.br@gmail.com>
2022-12-28 17:48:45 -0300

    00 - Módulo de conceitos iniciais

---
+++ 00_inicio/dados_3.js
@@ -0,0 +1,11 @@
+let idade = 20;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 7.876;
+console.log(typeof salario);
+
+let estaChovendo = false;
+console.log(typeof estaChovendo);
+
+
---
+++ 00_inicio/desafio_area.js
@@ -0,0 +1,9 @@
+const PI = 3.14;
+
+let raio = 10;
+
+let areaCir = PI * raio * raio;
+
+console.log(` The value of area is ${areaCir}`);
+// ou
+console.log(Math.PI);
