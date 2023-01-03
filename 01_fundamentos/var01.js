d9e752183fc30cbfd9f92cd186afe165cbef6c51
Patrícia Souza <patricia.sz.br@gmail.com>
2023-01-03 15:31:44 -0300

    Declaracoes de variaveis usando var, let e const

---
+++ 01_fundamentos/var01.js
@@ -0,0 +1,25 @@
+{
+  {
+    {
+      {
+        {
+          {
+            {
+              var sera = 'Será?';
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+console.log(sera);
+
+function teste() {
+  var local = 123;
+  console.log(local);
+}
+
+teste();
+console.log(local);
