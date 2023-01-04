f8a7ccda3697c3385015d34fdc9c724184b1909d
Patrícia Souza <patricia.sz.br@gmail.com>
2023-01-04 13:48:25 -0300

    Usando var, let em loop for

--- 01_fundamentos/usandoVarEmLoop02.js
+++ 01_fundamentos/usandoVarEmLoop02.js
@@ -6,8 +6,13 @@
     3. Incremento. 
 */
 
-for (let i = 0; i < 10; i++) {
-  console.log(i);
+const funcs = [];
+
+for (var i = 0; i < 10; i++) {
+  funcs.push(function () {
+    console.log(i);
+  });
 }
 
-console.log('i = ', i);
+funcs[2]();
+funcs[8]();
