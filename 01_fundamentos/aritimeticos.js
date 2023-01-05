5806b9f0690a55050f606ffdd9e5581c4531e108
Patrícia Souza <patricia.sz.br@gmail.com>
2023-01-05 16:26:54 -0300

    Operadores aritimeticos

---
+++ 01_fundamentos/aritimeticos.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [1, 4, 8, 2];
+
+const soma = a + b + c + d;
+const subtracao = d - c;
+const multiplicacao = c * a;
+const divisao = b / d;
+const modulo = c % 2;
+
+console.log(soma, subtracao, divisao, multiplicacao, modulo);
