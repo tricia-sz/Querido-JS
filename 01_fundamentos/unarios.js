6e427adf6aa673e4e3a9f289f6a1cfbdc5b94d77
Patrícia Souza <patricia.sz.br@gmail.com>
2023-01-06 15:11:35 -0300

    Operadores unários

---
+++ 01_fundamentos/unarios.js
@@ -0,0 +1,12 @@
+let numero1 = 1;
+let numero2 = 2;
+
+numero1++;
+console.log(numero1);
+
+--numero1;
+console.log(numero1);
+
+console.log(++numero1 === numero2--); // true
+
+console.log(numero1 === numero2); // false
