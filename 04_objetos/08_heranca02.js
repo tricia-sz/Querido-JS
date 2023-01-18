cd693aaaa94d6dbe83ea4b39c1fc82e26e00cc28
Patrícia Souza <patricia.sz.br@gmail.com>
2023-01-18 15:19:41 -0300

    Herança 02

--- 04_objetos/08_heranca02.js
+++ 04_objetos/08_heranca02.js
@@ -9,3 +9,43 @@ console.log(filho.attr1);
 console.log(filho.attr2);
 console.log(filho.attr3);
 console.log(filho.attr0);
+
+const carro = {
+  velAtual: 0,
+  velMax: 200, 
+  acelerarMais(delta) {
+    if(this.velAtual + delta <= this.velMax) {
+      this.velAtual += delta
+    } else {
+      this.velAtual = this.velMax
+    }
+  },
+  status(){
+    return `${this.velAtual}km/h de ${this.velMax}km/h`
+  }
+}
+
+const ferrari = {
+  modelo: 'F40',
+  velMax: 324 // shadowing
+}
+
+const volvo = {
+  modelo: 'V3',
+  status(){
+    return `${thius.modelo}km/h de ${super.status()}km/h`
+  }
+}
+
+Object.setPrototypeOf(ferrari, carro) // Relacao estabelecida entre ferrari e carro. Primeiro voce tem o objeto depois o prototipo
+Object.setPrototypeOf(volvo, carro) 
+
+console.log(ferrari);
+console.log(volvo);
+
+volvo.acelerarMais(100)
+console.log(volvo.status());
+
+ferrari.acelerarMais(300)
+console.log(ferrari.status());
+
